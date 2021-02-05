afff93090c420b848eb7320c63951f18ade9f342
davinchicoronado <david.coronado@mail.escuelaing.edu.co>
2021-02-05 17:51:31 -0500

    prueba

---
+++ src/main/java/edu/escuelaing/arem/ASE/app/SparkWebApp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arem.ASE.app;
+
+/**
+ *
+ * @author David Coronado
+ */
+import static spark.Spark.*;
+public class SparkWebApp {
+    
+    
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello Heroku");
+    }
+    static int getPort() {
+            if (System.getenv("PORT") != null) {
+                    return Integer.parseInt(System.getenv("PORT"));
+            }
+            return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
+ }
+}
