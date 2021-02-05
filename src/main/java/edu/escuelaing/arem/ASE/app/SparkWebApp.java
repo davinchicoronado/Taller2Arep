2959f6219892581f5005b252a5856f4267ca1e55
davinchicoronado <david.coronado@mail.escuelaing.edu.co>
2021-02-05 18:12:02 -0500

    port

--- src/main/java/edu/escuelaing/arem/ASE/app/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/ASE/app/SparkWebApp.java
@@ -14,7 +14,9 @@ public class SparkWebApp {
     
     
     public static void main(String[] args) {
+        port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
+        
     }
     static int getPort() {
             if (System.getenv("PORT") != null) {
